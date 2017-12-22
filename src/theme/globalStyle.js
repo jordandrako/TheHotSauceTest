9a7ea6b6ced7d5afb05a504b482c5c0f6a00a839
Jordan Janzen <jordanjanzen@jordanjanzen.com>
2017-12-22 11:37:23 -0700

    Pepper background with gradient overlay

--- src/theme/globalStyle.js
+++ src/theme/globalStyle.js
@@ -23,10 +23,20 @@ injectGlobal`
   body {
     font-family: ${typography.bodyFont};
     color: ${theme.textColor};
-    background: ${theme.siteBackground};
+    background: url(https://images.unsplash.com/reserve/MxJDaPQQAGf9MgAJNiLA_Peppers_Albuquerque_2013.JPG) no-repeat;
     line-height: 1.6;
     margin: 0;
     padding: 1em;
+    &:after {
+      content: '';
+      position: fixed;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      z-index: -1;
+      background-image: linear-gradient(-15deg, rgba(249, 111, 132,.7) 0%, rgba(254, 225, 64,.7) 100%);
+    }
   }
 
   img {

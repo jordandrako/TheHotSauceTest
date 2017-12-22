595a011f406f5982b6c37104340b489e1ec10d5b
Jordan Janzen <jordanjanzen@jordanjanzen.com>
2017-12-22 11:37:37 -0700

    Make header stand out from new background

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,13 +6,26 @@ const Head = styled.header`
 `;
 
 const Title = styled.h1`
+  font-size: 2.4em;
+  color: #ff512f;
+  text-shadow: 3px 3px 1px rgba(0, 0, 0, 0.7);
   transform: rotate(4deg);
+
   span:first-child {
     font-size: 0.6em;
+    text-shadow: 2px 2px 0.3px rgba(0, 0, 0, 0.7);
   }
+
   span:last-child {
     font-size: 1.6em;
+    text-shadow: 4px 4px 1.5px rgba(0, 0, 0, 0.7);
   }
+`;
+
+const SubTitle = styled.h2`
+  display: block;
+  padding: 0.3em;
+  background: #111;
 `;
 
 const Header = () => (
@@ -20,7 +33,7 @@ const Header = () => (
     <Title>
       <span>HOT</span> <span>HOT</span> <span>HOT</span>
     </Title>
-    <h2>CAN YOU HANDLE THE HEAT!?</h2>
+    <SubTitle>CAN YOU HANDLE THE HEAT!?</SubTitle>
   </Head>
 );
 
